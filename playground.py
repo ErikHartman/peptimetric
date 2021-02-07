d952a589d7446b0247bb3b67fbccae91ad28a06a
ErikHartman <50373207+ErikHartman@users.noreply.github.com>
2021-02-07 12:19:07 +0100

    add methods playground

--- playground.py
+++ playground.py
@@ -1,6 +1,8 @@
 import tkinter as tk
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+
 
 objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
 y_pos = np.arange(len(objects))
