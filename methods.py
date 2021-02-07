c7b79be8aa4a3fffd05423a4dd55d644ddc7863f
ErikHartman <50373207+ErikHartman@users.noreply.github.com>
2021-02-07 14:36:51 +0100

    added methods and playground

--- methods.py
+++ methods.py
@@ -7,3 +7,13 @@ def read_file(file):
     df = pd.DataFrame()
     df = pd.read_excel(file)
     return df
+
+def drop_zeros(df, colname):
+    df = df[df[colname] != 0]
+    return df
+
+def col_width(length, df, accession):
+    df = df.groupby(by=[accession]).sum()
+    n = len(df.index)
+    width = length/n
+    return width
--- playground.py
+++ playground.py
@@ -4,6 +4,28 @@ import numpy as np
 import pandas as pd
 import openpyxl
 from methods import *
+import os
 
-dataframe = read_file("example_files/peptide_sample_31.xlsx")
-print(dataframe.columns)
+folder_name="example_files"
+folder = os.listdir(folder_name)
+for file in folder:
+    df = read_file(folder_name+'/'+file)
+    print(file)
+
+
+df_protein = df.copy()
+df_protein=df_protein.groupby(by=['Accession','Peptide'], as_index=False).sum()
+
+print(df_protein.columns)
+print(len(df['Peptide'].index))
+print(len(df_protein['Accession'].index))
+df_protein = df_protein.sort_values('Area Sample 31', ascending=False)
+print(df_protein)
+df_protein = drop_zeros(df_protein, 'Area Sample 31')
+df_protien = df_protein.sort_values('Area Sample 31', ascending=False)
+
+intensity = np.log(df_protein['Area Sample 31'])
+label = range(len(intensity))
+print(len(intensity), len(label))
+plt.scatter(x=label, y=intensity)
+plt.show()
