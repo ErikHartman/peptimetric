9c487f91534c10eb99a26742e5c25be1bb03ad4a
simonmahdavi <simonmahdavi@msn.com>
2021-02-12 14:51:12 +0100

    fold change, rt, group

--- playground.py
+++ playground.py
@@ -1,3 +1,7 @@
+<<<<<<< Updated upstream
+=======
+import os
+>>>>>>> Stashed changes
 
 from methods import *
 from lists import *
@@ -6,7 +10,19 @@ from lists import *
 g1 = concatenate_dataframes(read_files())
 g2 = concatenate_dataframes(read_files())
 
+<<<<<<< Updated upstream
 master = g1.merge(g2, on=['Peptide','Accession'], how='outer', suffixes = ['_g1','_g2'])
 print(master.columns)
 create_venn(master)
 
+=======
+master = g1.merge(g2, on = ['Peptide','Accession'], how='outer', suffixes=['_g1', '_g2'])
+list_1 = create_protein_list(master)
+
+print(list_1[0].df)
+print(list_1[0].get_area_sum())
+
+
+
+
+>>>>>>> Stashed changes
