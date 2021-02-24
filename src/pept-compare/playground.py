ddac705cbe311aa01b451f1e875a967f664d8646
karvla <arvid94@gmail.com>
2021-02-24 17:40:58 +0100

    test: added first tests

--- src/pept-compare/playground.py
+++ src/pept-compare/playground.py
@@ -1,8 +1,8 @@
 from methods import *
 from lists import *
 
-g1 = concatenate_dataframes(read_files())
-g2 = concatenate_dataframes(read_files())
+g1 = concatenate_dataframes(read_files_gui())
+g2 = concatenate_dataframes(read_files_gui())
 master = g1.merge(g2, on=['Peptide', 'Accession'], how='outer', suffixes=['_g1', '_g2'])
 protein_list = create_protein_list(master)
 peptide_list = create_peptide_list(protein_list, 'P68871')
---
+++ tests/context.py
@@ -0,0 +1,8 @@
+import sys
+from pathlib import Path
+
+module_path = Path(__file__).parent.parent / "src/pept-compare"
+sys.path.insert(0, str(module_path.absolute()))
+
+# TODO: import other modules and make more test_module.py files
+import methods
