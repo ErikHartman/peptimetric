dbad5903a9f7fce1161469a2a0dd6b0730463d6f
ErikHartman <50373207+ErikHartman@users.noreply.github.com>
2021-03-03 21:22:34 +0100

    Threading
    @karvla  fick aldrig threading att funka tyvärr. playground.py innehåller kod som gör att en protein_scatter skapas. Klicka 2 ggr på ett protein så kör du create_peptide_graphic. Skrivit lite kommentarer i playground och methods för vägledning.,

--- src/pept-compare/playground.py
+++ src/pept-compare/playground.py
@@ -10,3 +10,9 @@ master = merge_dataframes(g1,g2)
 protein_list = create_protein_list(master)
 protein_list = apply_cut_off(protein_list, nbr_of_peptides=5, area=1000000, spectral_count=4)
 create_protein_scatter(protein_list, difference_metric='area_sum')
+
+"""
+Koden ovan skapar ett protein_scatter utifrån valda filer (välj två på måfå) från example_files
+Om du trycker på ett protein 2 ggr kommer create_peptide_graphic köras, men här blir plotten inte interaktiv
+vilket den blir om man endast kör create_peptide_graphic
+"""
