f4c7ff6e98742eabd642017821c1bcf42d04e57d
ErikHartman <50373207+ErikHartman@users.noreply.github.com>
2021-03-04 11:29:38 +0100

    update

--- UI_files/main.py
+++ UI_files/main.py
@@ -1,53 +1,20 @@
-################################################################################
-##
-## BY: WANDERSON M.PIMENTA
-## PROJECT MADE WITH: Qt Designer and PySide2
-## V: 1.0.0
-##
-################################################################################
-
 import sys
-import platform
-from PySide2 import QtCore, QtGui, QtWidgets
-from PySide2.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
-from PySide2.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtWidgets import QApplication
 
-# GUI FILE
 from ui_main import Ui_MainWindow
 
-# IMPORT FUNCTIONS
-from ui_functions import *
 
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
 
-        ## TOGGLE/BURGUER MENU
-        ########################################################################
-        self.ui.Btn_Toggle.clicked.connect(lambda: UIFunctions.toggleMenu(self, 250, True))
-
-        ## PAGES
-        ########################################################################
-
-        # PAGE 1
-        self.ui.btn_page_1.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_1))
-
-        # PAGE 2
-        self.ui.btn_page_2.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_2))
-
-        # PAGE 3
-        self.ui.btn_page_3.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_3))
-
-
-        ## SHOW ==> MAIN WINDOW
-        ########################################################################
-        self.show()
-        ## ==> END ##
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
--- UI_files/ui_functions.py
+++
@@ -1,4 +0,0 @@
-from main import *
-
-class UIFunctions(MainWindow):
-
