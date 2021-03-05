c4a4588bc547c6c3661f47aa1ae6a9d9466569ba
simonmahdavi <simonmahdavi@msn.com>
2021-03-05 14:13:10 +0100

    We have a server

--- src/pept-compare/webapp_main.py
+++ src/pept-compare/webapp_main.py
@@ -8,8 +8,25 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import sys, os
-cwd = "C:/Users/erikh/OneDrive/Dokument/GitHub/kand/src/peptcompare"
 
-from methods import *
+app = dash.Dash(__name__)
 
+# Import files 
 
+# App Layout 
+
+app.layout = html.Div([
+
+    html.H1("Visualizatition tool for proteomics ", style={'text-align': 'center'}), 
+
+    dcc.Textarea(
+        id='textarea-example',
+        placeholder='Serach protein...', 
+        value='Hello World',
+        style={'width': '40%'} , 
+   ),
+    html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line'})
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
