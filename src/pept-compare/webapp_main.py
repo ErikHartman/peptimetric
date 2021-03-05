5a8db540d9ccb9e1895fde0c7ed3370441c29889
ErikHartman <50373207+ErikHartman@users.noreply.github.com>
2021-03-05 15:02:34 +0100

    update webapp

--- src/pept-compare/webapp_main.py
+++ src/pept-compare/webapp_main.py
@@ -3,29 +3,65 @@ import pandas as pd
 import plotly.express as px 
 import plotly.graph_objects as go
 
-import dash  
+import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import sys, os
 
-app = dash.Dash(__name__)
+app = dash.Dash(external_stylesheets=[dbc.themes.SANDSTONE])
 
-# Import files 
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f8f8",
+}
 
-# App Layout 
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("PeptiDiff", className="display-4"),
+        html.Hr(),
+        html.H4("Visualization tool for proteomics and peptidomics", className="display-8"),
+        html.Hr(),
+        dbc.Nav([
+            dbc.NavLink("Menu 1", href="/", active="exact"),
+            dbc.NavLink("Menu 2", href="/menu-2", active="exact"),
+            dbc.NavLink("Menu 3", href="/menu-3", active="exact"),
+        ],
+        vertical=True,
+        pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
 
 app.layout = html.Div([
-
-    html.H1("Visualizatition tool for proteomics ", style={'text-align': 'center'}), 
+    dcc.Location(id="url"),
+    sidebar,
+    content,
+    
 
     dcc.Textarea(
         id='textarea-example',
         placeholder='Serach protein...', 
         value='Hello World',
-        style={'width': '40%'} , 
+        style={'width': '40%', 'height': 500, 'font_family': 'Roboto'} , 
    ),
     html.Div(id='textarea-example-output', style={'whiteSpace': 'pre-line'})
+    
 ])
 
 if __name__ == '__main__':
