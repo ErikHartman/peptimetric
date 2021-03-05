a683ec5884e9f8bde248bf8fc5a1daf5481a89cf
simonmahdavi <simonmahdavi@msn.com>
2021-03-05 12:09:27 +0100

    again

---
+++ webapp/main.py
@@ -0,0 +1,19 @@
+
+import pandas as pd
+import plotly.express as px 
+import plotly.graph_objects as go
+
+import dash  
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import sys
+
+sys.path.insert(1, "/src/pept-compare")
+from methods import *
+
+
+
+
+
+
