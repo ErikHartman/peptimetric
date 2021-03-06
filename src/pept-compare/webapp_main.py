3ca9cec1cf1990636ea4614d1385cc0b442f2638
ErikHartman <50373207+ErikHartman@users.noreply.github.com>
2021-03-06 18:45:03 +0100

    Update webapp_main.py

--- src/pept-compare/webapp_main.py
+++ src/pept-compare/webapp_main.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 import dash
+import dash_table
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
@@ -13,16 +14,18 @@ from dash.dependencies import Input, Output
 
 app = dash.Dash(external_stylesheets=[dbc.themes.SANDSTONE])
 
-NAVBAR_BRAND_STYLE = {
-    "font-size": "20px",
-    "margin-left": "5px" "!important"
- }
+NAVBAR_STYLE = {
+    "left": 0,
+    "background-color": "light",
+    
+    }
+
 SIDEBAR_STYLE = {
     "position": "fixed",
-    "top": "4rem",
+    "top": "2rem",
     "left": 0,
     "bottom": 0,
-    "width": "16rem",
+    "width": "12rem",
     "padding": "2rem 1rem",
     "background-color": "light",
 }
@@ -32,19 +35,38 @@ CONTENT_STYLE = {
     "margin-right": "2rem",
     "padding": "2rem 1rem",
 }
-navbar = dbc.NavbarSimple(
-    children=[
-        
-        dbc.NavItem(dbc.NavLink("Add files", href="#")),
-        dbc.NavItem(dbc.NavLink("Documentation", href="#")),
-        dbc.NavItem(dbc.NavLink("FAQ", href="#"))
-    ],
-    brand="PeptiDiff",
-    brand_style=NAVBAR_BRAND_STYLE,
-    brand_href="#",
-    color="light",
-    dark=False,
+navbar = dbc.Navbar(
+    [
+        dbc.NavbarBrand("Eriks och Simons kandidatarbete"),
+            dbc.Nav(
+            [
+                dbc.DropdownMenu(
+                    label="Files",
+                    children=[
+                        dbc.DropdownMenuItem("View files", href="/View-files"),
+                        dbc.DropdownMenuItem("Add files", href="/Add-files"),
+                    ],
+                    className="mr-1",
+                    nav=True,
+                    in_navbar=True,
+                ),
+                dbc.Button(
+                    "Export data",
+                    className="mr-1",
+                    color='info',
+                    href="/Export-data"
+                ),
+                dbc.NavLink("Documentation", href="/Documentation", active="exact"),
+                dbc.NavLink("FAQ", href="/FAQ", active="exact"),
+                dbc.NavLink("Feedback", href="/Feedback", active="exact"),
+            ],
+            navbar=True,
+            className="ml-auto",
+            style=NAVBAR_STYLE
+        ),   
+    ]
 )
+            
 
 sidebar = html.Div(
     [
@@ -57,17 +79,17 @@ sidebar = html.Div(
         ],
         vertical=True,
         pills=True,
+        className="mb-4"
         ),
     ],
     style=SIDEBAR_STYLE,
 )
-"""
-#TODO: Fix card layout
 
-card = dbc.Card(
+how_to_use_card = dbc.Card(
     dbc.CardBody(
         [
-            html.H4("Help", className="card-title"),
+            html.H4("How to use", className="card-title mb-4 font-weight-bold"),
+            html.Hr(),
             html.H6("Using PeptiDiff", className="card-subtitle"),
             html.P(
                 "Load files ",
@@ -75,38 +97,74 @@ card = dbc.Card(
             ),
         ]
     ),
-    style={"width": "18rem", "height":"30rem",  "align":'center'}, color='light'
+    style={'height':'22rem'},
+    color='white', className="border-dark mb-4"
 )
-"""
+
+protein_fig = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H4("Protein View", className="card-title mb-4 font-weight-bold"),
+            html.Hr(),
+        ]
+    ),
+    style={'height':'22rem'},
+    color='white', className="border-dark"
+)
+
+
+peptide_fig = dbc.Card(
+    dbc.CardBody(
+        [
+            html.H4("Peptide View", className="card-title mb-4 font-weight-bold"),
+            html.Hr(),
+        ]
+    ),
+    style={'height':'17rem'},
+    color='white', className="border-dark"
+)
+
+columns = ['Peptide','Start','End','Intensity 1',' Intensity 2']
+protein_info = dash_table.DataTable(
+        id='protein_info',
+        columns=[{"name": str(i), "id": str(i)} for i in columns],
+        style_header={
+        'fontWeight': 'bold',
+        'backgroundColor':'primary',
+    },
+    )
+
+peptide_info = dash_table.DataTable(
+        id='peptide_info',
+        columns=[{"name": str(i), "id": str(i)} for i in columns],
+        style_header={
+        'fontWeight': 'bold'
+    },
+    ),
+
 content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
 
-app.layout = html.Div([
+app.layout = dbc.Container([
     dcc.Location(id="url"),
-    navbar,
-    sidebar,
-    content,
+    #Navbar
+    dbc.Row([
+        dbc.Col(navbar, width={"size":12}, className="mb-4"),
+    ]),
+    #Protein and how to row
+    dbc.Row([
+        dbc.Col(sidebar, width={'size':0}),
+        dbc.Col(protein_fig, width={'size':5}),
+        dbc.Col(protein_info, width={'size':1}),
+        dbc.Col(how_to_use_card , width={'size':3, "offset":3}),
+    ]),
+    #Peptide row
+    dbc.Row([
+        dbc.Col(peptide_fig, width={'size': 8}),
+        dbc.Col(peptide_info, width={'size':2})
+    ])
     
 ])
-@app.callback(
-    Output("page-content", "children"),
-    [Input("url", "pathname")]
-)
-def render_menu_content(pathname):
-    if pathname == "/":
-        return [
-                html.H1('Menu 1',
-                        style={'textAlign':'center'}),
-                ]
-    elif pathname == "/menu-2":
-        return [
-                html.H1('Menu 2',
-                        style={'textAlign':'center'}),
-                ]
-    elif pathname == "/menu-3":
-        return [
-                html.H1('Menu 3',
-                        style={'textAlign':'center'}),
-        ]
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
