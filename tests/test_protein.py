71f23687d4d7aa07a555dcb5388b18e79e03da9f
karvla <arvid94@gmail.com>
2021-04-16 19:33:34 +0200

    fix: broken test

--- tests/test_protein.py
+++ tests/test_protein.py
@@ -1,8 +1,6 @@
 import pytest
 from pathlib import Path
 from methods import concatenate_dataframes
-from methods import create_protein_scatter
-from methods import apply_cut_off
 from lists import create_protein_list
 from methods import make_peptide_dfs
 from playground import merge_dataframes
@@ -15,7 +13,6 @@ def test_protein():
     g2 = concatenate_dataframes(make_peptide_dfs([file2]))
     master = merge_dataframes(g1,g2)
     protein_list = create_protein_list(master)
-    protein_list = apply_cut_off(protein_list, nbr_of_peptides=5, area=1000000, spectral_count=4)
 
 
 
